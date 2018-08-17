Fixed regulation covariance (to avoid different output beetween GLLiM and jGLLiM)

Core/gllim.py
@@ -80,10 +80,8 @@ class MyGMM(GaussianMixture):
 
     def _m_step(self, Y, log_resp):
         super()._m_step(Y, log_resp)
-        self._m_step_callback(Y)
-
-    def _m_step_callback(self, Y):
         self.current_iter_ll.append(self.log_likelihood(Y))
+
 
     def log_likelihood(self, Y):
         vec_log_prob, _ = self._estimate_log_prob_resp(Y)
@@ -92,10 +90,8 @@ class MyGMM(GaussianMixture):
     def _print_verbose_msg_iter_end(self, n_iter, diff_ll):
         if self.track:
             self.track_params.append((self.weights_, self.means_, self.full_covariances_))
-        if self.verbose == 1:
+        if self.verbose >= 0:
             logging.debug(f"Iteration {n_iter} : Log-likelihood = {self.current_iter_ll[-1]}")
-        elif self.verbose == 0:
-            logging.debug(f"Iteration {n_iter}")
 
     def _print_verbose_msg_init_end(self, ll):
         super()._print_verbose_msg_init_end(ll)
@@ -108,17 +104,19 @@ class MyGMM(GaussianMixture):
         return get_full_covariances(self.covariances_, self.covariance_type, K, N_features)
 
 
+DEFAULT_REG_COVAR = 1e-08
+
 class GLLiM():
     ''' Gaussian Locally-Linear Mapping'''
 
     def __init__(self, K_in, Lw=0, sigma_type='iso', gamma_type='full',
-                 verbose=True):
+                 verbose=True, reg_covar=DEFAULT_REG_COVAR):
 
         self.K = K_in
         self.Lw = Lw
         self.sigma_type = sigma_type
         self.gamma_type = gamma_type
-        self.reg_covar = 1e-08
+        self.reg_covar = reg_covar
         self.verbose = verbose
         self.track_theta = False
         self.nb_init_GMM = 1  # Number of init made by GMM when fit is init with it
@@ -234,19 +232,7 @@ class GLLiM():
             logging.debug("--- {} seconds for EM initialization---".format(time.time() - start_time_EMinit))
         return rnk
 
-    def init_fit(self, T, Y, init):
-        """Initialize model parameters. Three cases are supported :
-            - init = 'kmeans' :  GMM initialization, itself with kmeans initialization
-            - init = None : initialization with basic values (zeros, identity)
-            - init = 'random' : GMM initialization, itself with random initialization
-            - init = rnk : Array of clusters probabilities : skip GMM init.
-            - init = theta , where theta is a dict of Gllim parameters (with Sigma shape compatible with sigmae_type)
-        Remark : At the end, all that matter are rnk, since fit start by maximization.
-        """
-        init = init or ()
-        self.Lt = T.shape[1]
-        self.D = Y.shape[1]
-
+    def _default_init(self):
         # Add S covariances
         self.SkList_W = np.zeros((self.K, self.Lw, self.Lw))
 
@@ -278,6 +264,22 @@ class GLLiM():
         else:
             raise CovarianceTypeError
 
+
+    def init_fit(self, T, Y, init):
+        """Initialize model parameters. Three cases are supported :
+            - init = 'kmeans' :  GMM initialization, itself with kmeans initialization
+            - init = None : initialization with basic values (zeros, identity)
+            - init = 'random' : GMM initialization, itself with random initialization
+            - init = rnk : Array of clusters probabilities : skip GMM init.
+            - init = theta , where theta is a dict of Gllim parameters (with Sigma shape compatible with sigmae_type)
+        Remark : At the end, all that matter are rnk, since fit start by maximization.
+        """
+        init = init or ()
+        self.Lt = T.shape[1]
+        self.D = Y.shape[1]
+
+        self._default_init()
+
         if init in ['random', 'kmeans']:
             self.rnk = self._T_GMM_init(T, init)
 
@@ -285,6 +287,7 @@ class GLLiM():
             if self.verbose:
                 logging.debug('Initialization with given rnk')
             self.rnk = np.array(init['rnk'])
+            self._rnk_init = np.array(self.rnk)
             assert self.rnk.shape == (T.shape[0], self.K)
         elif type(init) is dict:
             self._init_from_dict(init)
@@ -528,6 +531,7 @@ class GLLiM():
             logging.info("{} initialization... (N = {}, L = {} , D = {}, K = {})".format(self.__class__.__name__,
                                                                                          N, L, D, self.K))
         self.init_fit(T, Y, init)
+
         if self.verbose is not None:
             logging.info("Done. GLLiM fitting...")
         self.current_iter = 0
@@ -569,9 +573,7 @@ class GLLiM():
 
     def end_iter_callback(self, loglikelihood):
         if self.verbose is not None:
-            logging.debug(f"Iteration {self.current_iter}")
-        elif self.verbose:
-            logging.debug(f"Log-likelihood = {loglikelihood} at iteration nb : {self.current_iter}")
+            logging.debug(f"Iteration {self.current_iter} : Log-likelihood = {loglikelihood} ")
 
         self.LLs_.append(loglikelihood)
         if self.track_theta:  # Save parameters history
@@ -601,6 +603,8 @@ class GLLiM():
             elif self.sigma_type == 'full':
                 i = _inv_sym_def(sig)
                 i = np.dot(i, Ak)
+            else:
+                raise CovarianceTypeError
 
             if np.allclose(Ak, np.zeros((self.D, self.L))):
                 sigS = gam
@@ -886,13 +890,19 @@ class jGLLiM(GLLiM):
         ])
         return {"rho": rho, "m": m, "V": V}
 
-    def _get_GMM(self,maxIter,rho,m,precisions,verbose):
-        Gmm = MyGMM(n_components=self.K,n_init=1,max_iter=maxIter,
-                    tol=np.finfo(np.float64).eps,
-                    weights_init=rho,means_init=m,precisions_init=precisions,
-                    verbose=verbose,track=self.track_theta)
-        return Gmm
+    def _Gmm_setup(self, T, Y, maxIter):
+        first_theta = self.compute_next_theta(T, Y)  # theta from rnk
+        jGMM_params = self.GLLiM_to_GGM(*first_theta)
+        precisions_chol = _compute_precision_cholesky(jGMM_params["V"], "full")
+        precisions = np.matmul(precisions_chol, precisions_chol.transpose((0, 2, 1)))
+        TY = np.concatenate((T, Y), axis=1)
 
+        verbose = {None: -1, False: 0, True: 1}[self.verbose]
+        Gmm = MyGMM(n_components=self.K, n_init=1, max_iter=maxIter, reg_covar=self.reg_covar,
+                    tol=np.finfo(np.float64).eps,
+                    weights_init=jGMM_params["rho"], means_init=jGMM_params["m"], precisions_init=precisions,
+                    verbose=verbose, track=self.track_theta)
+        return TY, Gmm
 
     def fit(self, T, Y, init, maxIter=100):
         """Use joint GMM model
@@ -908,17 +918,13 @@ class jGLLiM(GLLiM):
             logging.info("{} initialization... (N = {}, L = {} , D = {}, K = {})".format(self.__class__.__name__,
                                                                                          N, L, D, self.K))
         self.init_fit(T, Y, init)
+        TY, Gmm = self._Gmm_setup(T, Y, maxIter - 1)
+
         if self.verbose is not None:
             logging.info("Done. jGMM fitting...")
 
         start_time_EM = time.time()
 
-        jGMM_params = self.GLLiM_to_GGM(*self.compute_next_theta(T,Y)) # theta from rnk
-        precisions_chol = _compute_precision_cholesky(jGMM_params["V"], "full")
-        precisions = np.matmul(precisions_chol, precisions_chol.transpose((0, 2, 1)))
-        TY = np.concatenate((T, Y), axis=1)
-        verbose = {None: -1, False: 0, True: 1}[self.verbose]
-        Gmm = self._get_GMM(maxIter,jGMM_params["rho"],jGMM_params["m"],precisions,verbose)
         Gmm.fit(TY)
         self.LLs_ = Gmm.log_likelihoods[0]
 
@@ -927,14 +933,17 @@ class jGLLiM(GLLiM):
             logging.info("--- {} mins, {} secs for fit ---".format(t // 60, t - 60 * (t // 60)))
 
         if self.track_theta:
-            def tolist(rho, m, V):
-                params = self.GMM_to_GLLiM(rho, m, V, self.L)
-                return {c: v.tolist() for c, v in params.items()}
+            self.track = self.track_from_gmm(Gmm)
 
-            self.track = [tolist(rho, m, V) for (rho, m, V) in Gmm.track_params]
         rho, m, V = Gmm.weights_, Gmm.means_, Gmm.covariances_
 
         t = time.time()
         self._init_from_dict(self.GMM_to_GLLiM(rho, m, V, self.L))
         if self.verbose is not None:
             logging.debug("--- {:.3f} s to compute correspondance".format(time.time() - t))
+
+    def track_from_gmm(self, Gmm):
+        tolist = lambda rho, m, V: {c: v.tolist() for c, v in
+                                    self.GMM_to_GLLiM(rho, m, V, self.L).items()}
+
+        return [tolist(rho, m, V) for (rho, m, V) in Gmm.track_params]

Core/training.py
@@ -26,7 +26,8 @@ NB_MAX_ITER_SECOND = 20
 """Number of used processes"""
 PROCESSES = 4
 
-warnings.filterwarnings("ignore")
+
+# warnings.filterwarnings("ignore")
 
 
 def initialize_process(_Ttrain, _Ytrain):
@@ -111,6 +112,7 @@ def init_local(Ttrain, Ytrain, K, ck_init_function, precision_rate, Lw = 0, sigm
     if track_theta:
         gllim.start_track()
     gllim.fit(Ttrain, Ytrain, {"rnk": rnk}, maxIter=NB_MAX_ITER)
+    np.random.seed()
     return gllim
 
 
@@ -124,16 +126,18 @@ def multi_init(Ttrain, Ytrain, K, Lw = 0, sigma_type = "iso", gamma_type = "full
     if track_theta:
         gllim.start_track()
     gllim.fit(Ttrain, Ytrain, {"rnk": rnk}, maxIter=NB_MAX_ITER)
+    np.random.seed()
     return gllim
 
 
 def basic_fit(Ttrain, Ytrain, K, Lw = 0, sigma_type = "iso", gamma_type = "full",
-              track_theta = False, gllim_cls = GLLiM, verbose=False) :
-    """Performs one fit with random GMM initialisation"""
+              track_theta=False, gllim_cls=GLLiM, verbose=False, rnk_init=None):
+    """Performs one fit, with random GMM initialisation unless rnk_init is given"""
     gllim = gllim_cls(K, Lw, sigma_type=sigma_type, gamma_type=gamma_type, verbose=verbose)
     if track_theta:
         gllim.start_track()
-    gllim.fit(Ttrain, Ytrain, 'random', maxIter=NB_MAX_ITER)
+    init = {'rnk': rnk_init} if rnk_init is not None else 'random'
+    gllim.fit(Ttrain, Ytrain, init, maxIter=NB_MAX_ITER)
     return gllim
 
 

experiences/plots.py
@@ -180,7 +180,7 @@ def init_cos():
 
     x = np.array([0.55])
     y = exp.context.F(x[None, :])
-    exp.mesures.illustration(gllim, x, y, savepath=PATHS("init_cos1.png"))
+    exp.mesures.illustration(gllim, x, None, savepath=PATHS("init_cos1.png"))
 
     gllim = exp.load_model(100, mode=RETRAIN and "r" or "l", track_theta=False, init_local=100,
                            gamma_type="full", gllim_cls=GLLiM)
@@ -227,10 +227,10 @@ def comparaison_MCMC():
 def main():
     # exemple_pre_lissage()
     # plot_estimeF_simple()
-    plot_estimeF()
+    # plot_estimeF()
     # plot_evo_LL()
     # plusieurs_K_N(20)
-    # init_cos()
+    init_cos()
     # regularization()
     # comparaison_MCMC()
 

experiences/test.py
@@ -204,14 +204,18 @@ def setup_jGLLiM_GLLiM():
 
 
 def equivalence_jGLLiM_GLLIM():
-    X = np.random.multivariate_normal(np.zeros(5) + 0.2, np.eye(5), 2000)
-    Y = np.random.multivariate_normal(np.zeros(6) + 10, np.eye(6), 2000)
+    h = HapkeContext((0, 1, 2, 3))
+    X = h.get_X_sampling(2000)
+    Y = np.random.multivariate_normal(np.zeros(10) + 0.4, np.eye(10), 2000)
 
-    gllim = GLLiM(10, 0, sigma_type="full", gamma_type="full", verbose=True)
-    gllim.fit(X, Y, None, maxIter=100)
+    gllim = GLLiM(10, 0, sigma_type="full", gamma_type="full", verbose=False)
+    gllim.init_fit(X, Y, "random")
+    rnk = gllim.rnk
+    init = {"rnk": rnk}
+    gllim.fit(X, Y, init, maxIter=5)
 
-    jgllim = jGLLiM(10, 0, sigma_type="full", gamma_type="full", verbose=True)
-    jgllim.fit(X, Y, None, maxIter=99)
+    jgllim = jGLLiM(10, 0, sigma_type="full", gamma_type="full", verbose=False)
+    jgllim.fit(X, Y, init, maxIter=5)
 
     theta = (gllim.pikList, gllim.ckList, gllim.GammakList, gllim.AkList, gllim.bkList, gllim.full_SigmakList)
     jtheta = (jgllim.pikList, jgllim.ckList, jgllim.GammakList, jgllim.AkList, jgllim.bkList, jgllim.full_SigmakList)
@@ -258,10 +262,10 @@ if __name__ == '__main__':
     # graphiques.plot_Y(Y)qw
     # simple_function()
     # evolu_cluster()
-    # equivalence_jGLLiM_GLLIM()  # OK
+    equivalence_jGLLiM_GLLIM()  # OK
     # test_dF()
     # _compare_Fsym()   #OK 27 /6 /2018
     # test_map()
     # plusieurs_K_N(False,imax=200,Nfixed=False,Kfixed=False)
     # compare_R(sigma_type="full",gamma_type="iso")
-    details_convergence(60, True)
+    # details_convergence(60, True)

plotting/graphiques.py
@@ -168,19 +168,16 @@ class doubleplusieursKN(plusieursKN):
         return [c for i in np.linspace(0, 1, nb // 2) for c in [cm.rainbow(i), cm.rainbow(i)]]
 
 
-
-
-def _axe_schema_1D_direct(axe, ck, ckS, Ak, bk, xlims):
-    s = axe.scatter(ck, ckS, marker="+", color='r', label="$(c_{k}, c_{k}^{*})$")
+def _axe_schema_1D_direct(axe, ck, ckS, Ak, bk, xlims, labelcks=True):
 
     x_box = np.linspace(0, (xlims[1] - xlims[0]) / 50, 100)
-    artists = [s]
     for k, a, b in zip(range(len(bk)), Ak[:, 0, 0], bk[:, 0]):
         x = x_box + ck[k]
         y = a * x + b
-        p = axe.plot(x, y, color="g", alpha=0.7)
-        artists.extend(p)
-    return artists
+        label = "$a_{k}$" if k == 0 else None
+        axe.plot(x, y, color="g", alpha=0.7, label=label)
+
+    axe.scatter(ck, ckS, marker="+", color='r', label="$(c_{k}, c_{k}^{*})$" if labelcks else None)
 
 class schema_1D(abstractDrawerMPL):
     Y_TITLE_BOX_WITH_CONTEXT = 1.1
@@ -189,29 +186,34 @@ class schema_1D(abstractDrawerMPL):
     def _format_context(context):
         p = context["init_local"]
         if p is None:
-            return f"Initialisation usuelle. Après apprentissage, $\max\limits_{{k}} \Gamma_{{k}}$ = {context['max_Gamma']:.1e}"
+            return f"Initialisation usuelle (K = { context['K'] }). Après apprentissage, $\max\limits_{{k}} \Gamma_{{k}}$ = {context['max_Gamma']:.1e}"
         else:
-            return f"Initialisation avec précision = {p}. Après apprentissage, $\max\limits_{{k}} \Gamma_{{k}}$ = {context['max_Gamma']:.1e}"
+            return f"Initialisation avec précision = {p} (K = { context['K'] }). Après apprentissage, $\max\limits_{{k}} \Gamma_{{k}}$ = {context['max_Gamma']:.1e}"
 
     def main_draw(self, points_true_F, ck, ckS, Ak, bk, xlims, xtrue, ytest, modal_preds):
         axe = self.fig.add_subplot(2, 1, 1)
         axe.set_xlim(*xlims)
 
         axe.plot(*points_true_F, color="b", label="True F")
-        _axe_schema_1D_direct(axe, ck, ckS, Ak, bk, xlims)
-
+        _axe_schema_1D_direct(axe, ck, ckS, Ak, bk, xlims, labelcks=ytest is None)
+        lhl = axe.get_legend_handles_labels()
+        bbox = (0.95, 0.5)
         if ytest is not None:
             axe = self.fig.add_subplot(2, 1, 2)
             axe.scatter(ck, ckS, marker="+", color='r', label="$(c_{k}, c_{k}^{*})$")
             axe.plot(*points_true_F, color="b", label="True F")
 
-            axe.axhline(y=ytest)
             for i, (xpred, w) in enumerate(modal_preds):
                 axe.axvline(xpred[0], color="y", linewidth=0.5, label="{0:.4f} - {1:.2f}".format(xpred[0], w))
                 axe.annotate(str(i), (xpred[0], 0))
+
+            axe.axhline(y=ytest, label="$y_{obs}$")
             if xtrue is not None:
                 axe.axvline(xtrue, linestyle="--", color="g", alpha=0.5, label="$x_{initial}$")
-        self.fig.legend(*axe.get_legend_handles_labels(), bbox_to_anchor=(1.01, 0.8))
+            lh, ll = axe.get_legend_handles_labels()
+            lhl = [lhl[0][-1]] + lh, [lhl[1][-1]] + ll
+            bbox = (1.01, 0.8)
+        self.fig.legend(*lhl, bbox_to_anchor=bbox)
         self.fig.subplots_adjust(right=0.77)
 
 

tools/measures.py
@@ -78,7 +78,7 @@ class Mesures():
     def _nrmse_compare_F(self, gllim):
         """if clean is given, remove theoritically absurd values and returns additional nrmse"""
         exp = self.experience
-        X = exp.context.get_X_sampling(exp.Ntest)
+        X = exp.Xtest
         Y_vrai = exp.context.F(X)
 
         Y_estmean, _ = exp.reconstruct_F(gllim, X)
@@ -521,12 +521,15 @@ class VisualisationMesures(Mesures):
         xlim = self.experience.context.XLIMS[0]
         x = np.linspace(*xlim, N)
         y = self.experience.context.F(x[:, None])
-        modals, _, weights = gllim.modal_prediction(ytest, components=10, sort_by="weight")
-        X = modals[0]
-        modals = list(zip(X, weights[0]))
+        modals = ()
+        if ytest is not None:
+            modals, _, weights = gllim.modal_prediction(ytest, components=10, sort_by="weight")
+            X = modals[0]
+            modals = list(zip(X, weights[0]))
+            ytest = ytest[0, 0]
         savepath = savepath or self.experience.archive.get_path("figures", filecategorie="schema")
         context = dict(**self.experience.get_infos(), max_Gamma=gllim.GammakList.max())
-        self.G.schema_1D((x, y), gllim.ckList, gllim.ckListS, gllim.AkList, gllim.bkList, xlim, xtrue[0], ytest[0, 0],
+        self.G.schema_1D((x, y), gllim.ckList, gllim.ckListS, gllim.AkList, gllim.bkList, xlim, xtrue[0], ytest,
                          modals,
                          context=context, write_context=True, draw_context=False,
                          title="", savepath=savepath)