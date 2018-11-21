838c8d093b500688160cc0efc2f781d18f682df2
BeKUGLER <benoit.kugler@inria.fr>
2018-11-21 11:05:47 +0100

    Ajout stabilité numérique

--- Core/cython/noise_em.pyx
+++ Core/cython/noise_em.pyx
@@ -4,11 +4,15 @@ cimport openmp
 cimport numpy as np
 import numpy as np
 from libc.math cimport isfinite, exp
+from libc.stdio cimport printf
 from cython.parallel import prange
 
 include "probas.pyx"
 
 cdef Py_ssize_t NUM_THREADS = multiprocessing.cpu_count()
+
+
+# --------------------------- Helpers --------------------------- #
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
@@ -91,35 +95,7 @@ cdef void mu_helper_clean(const double[:,:] FXsi, const long[:] maski,
         scalar_mult_vect( 1. / sum, tmp_mu)
 
 
-
-@cython.boundscheck(False)
-@cython.wraparound(False)
-def mu_step_NoIS(const double[:,:] Yobs, const double[:,:,:] FXs, const long[:,:] mask):
-    cdef Py_ssize_t Ny = FXs.shape[0]
-    cdef Py_ssize_t Ns = FXs.shape[1]
-    cdef Py_ssize_t D =  FXs.shape[2]
-
-    tmp_muNp = np.zeros(D)
-    wsNp = np.ones((Ny,Ns)) / Ns
-    maximal_muNp = np.zeros(D)
-
-    cdef double[:] tmp_mu = tmp_muNp
-    cdef double[:,:] ws = wsNp
-    cdef double[:] maximal_mu = maximal_muNp
-
-
-    cdef Py_ssize_t i, j, d
-    cdef double sum = 0
-
-    for i in range(Ny):
-        mu_helper_clean(FXs[i],mask[i],ws[i],Yobs[i], tmp_mu)
-
-        for d in range(D):
-            maximal_mu[d] += tmp_mu[d]
-
-    scalar_mult_vect(1. / Ny,maximal_mu)
-    return maximal_muNp
-
+# ------------------------------ Runners (C funcs) ------------------------------ #
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
@@ -161,23 +137,6 @@ cdef void _run_sigma_step_full(const double[:,:] Yobs, const double[:,:,:] FXs,
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
-def sigma_step_full_NoIS(const double[:,:] Yobs, const double[:,:,:] FXs, const long[:,:] mask, const double[:] maximal_mu):
-    cdef Py_ssize_t Ny = FXs.shape[0]
-    cdef Py_ssize_t Ns = FXs.shape[1]
-    cdef Py_ssize_t D =  FXs.shape[2]
-
-    tmp_sigma = np.zeros((D, D))
-    ws = np.ones((Ny,Ns)) / Ns
-    maximal_sigma = np.zeros((D, D))
-    tmp = np.zeros(D)
-
-    _run_sigma_step_full(Yobs, FXs, mask, maximal_mu, ws, tmp, tmp_sigma, maximal_sigma)
-
-    return maximal_sigma
-
-
-@cython.boundscheck(False)
-@cython.wraparound(False)
 cdef void _run_sigma_step_diag(const double[:,:] Yobs, const double[:,:,:] FXs,
                                const long[:,:] mask, const double[:] maximal_mu,
                                const double[:,:] ws, double[:] tmp_sigma, double[:] sigma_out) nogil:
@@ -210,45 +169,7 @@ cdef void _run_sigma_step_diag(const double[:,:] Yobs, const double[:,:,:] FXs,
     scalar_mult_vect(1. / Ny, sigma_out)
 
 
-@cython.boundscheck(False)
-@cython.wraparound(False)
-def sigma_step_diag_NoIS(const double[:,:] Yobs, const double[:,:,:] FXs, const long[:,:] mask, const double[:] maximal_mu):
-    cdef Py_ssize_t Ny = FXs.shape[0]
-    cdef Py_ssize_t Ns = FXs.shape[1]
-    cdef Py_ssize_t D =  FXs.shape[2]
-
-    esp_sigmaNp = np.zeros(D)
-    wsNp = np.ones((Ny,Ns)) / Ns
-    maximal_sigmaNp = np.zeros(D)
-    tmp_sigmaNp = np.zeros(D)
-
-    # for i in range(Ny):
-    #     tmp = 0
-    #     sum = 0
-    #     for d in range(D):
-    #         esp_sigma[d] = 0
-    #
-    #     for j in range(Ns):
-    #
-    #         if (not mask[i,j]) and isfinite(wsi[i,j]) and isVfinite(FXs[i,j]):
-    #             sum += wsi[i,j]
-    #
-    #             for d in range(D):
-    #                 tmp = FXs[i,j,d] + maximal_mu[d] - Yobs[i,d]
-    #                 tmp = tmp ** 2
-    #                 esp_sigma[d] += wsi[i,j] * tmp
-    #
-    #     if not sum == 0:
-    #         scalar_sum_vect(esp_sigma, 1. / sum, maximal_sigma)
-    #
-    # scalar_mult_vect(1. / Ny, maximal_sigma)
-
-    _run_sigma_step_diag(Yobs, FXs, mask, maximal_mu, wsNp, tmp_sigmaNp, maximal_sigmaNp)
-
-    return maximal_sigmaNp
-
-
-# ------------------------------ WITH IS ------------------------------ #
+#   WITH IS
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
@@ -264,9 +185,14 @@ cdef void _helper_mu(const double[:,:] X, const double[:] weights, const double[
     cdef Py_ssize_t Ns = out_weigths.shape[0]
 
     cdef Py_ssize_t n
+    cdef double min_log = 0
+
+    for n in range(Ns):   # re scaling to avoid numerical issue
+        if log_p_tilde[n] < min_log:
+            min_log = log_p_tilde[n]
 
     for n in range(Ns):
-        out_weigths[n] = exp(log_p_tilde[n]) / out_weigths[n] # Calcul des poids
+        out_weigths[n] = exp(log_p_tilde[n] - min_log) / out_weigths[n] # Calcul des poids
 
     mu_helper_clean(FX, mask_x, out_weigths, y, out_mu)
 
@@ -283,9 +209,10 @@ cdef void _run_C_mu_step_diag_IS(const double[:,:] Yobs, const double[:,:,:] Xs,
                                     double[:,:] tmp_mu, double[:] maximal_mu, double[:,:] ws,
                                     double[:,:,:] tmp_KNs, double[:,:,:] tmp_KL) nogil:
     cdef Py_ssize_t Ny = FXs.shape[0]
+    cdef Py_ssize_t Ns = FXs.shape[1]
     cdef Py_ssize_t D =  FXs.shape[2]
 
-    cdef Py_ssize_t i, d, thread_number
+    cdef Py_ssize_t i, d, thread_number, ns
 
     for i in prange(Ny, nogil=True, num_threads=NUM_THREADS, schedule='static'):
         thread_number = openmp.omp_get_thread_num()
@@ -293,7 +220,8 @@ cdef void _run_C_mu_step_diag_IS(const double[:,:] Yobs, const double[:,:,:] Xs,
         for d in range(D):
             y_moins_mu_view[thread_number,d] = Yobs[i,d] - current_mean[d]
 
-        loggauspdf_diag(FXs[i],y_moins_mu_view[thread_number],current_cov, log_p_tilde[thread_number])
+        loggauspdf_diag(FXs[i],y_moins_mu_view[thread_number], current_cov, log_p_tilde[thread_number])
+
         _helper_mu(Xs[i], weightss[i], meanss[i], gllim_chol_covs, log_p_tilde[thread_number],
                    FXs[i], mask[i], Yobs[i], tmp_mu[thread_number], ws[i],
                    tmp_KNs[thread_number], tmp_KL[thread_number])
@@ -302,38 +230,6 @@ cdef void _run_C_mu_step_diag_IS(const double[:,:] Yobs, const double[:,:,:] Xs,
             maximal_mu[d] += tmp_mu[thread_number,d]
 
     scalar_mult_vect(1. / Ny,maximal_mu)
-
-@cython.boundscheck(False)
-@cython.wraparound(False)
-def mu_step_diag_IS(const double[:,:] Yobs, const double[:,:,:] Xs, const double[:,:,:] meanss,
-                    const double[:,:] weightss, const double[:,:,:] FXs, const long[:,:] mask,
-                    const double[:,:,:] gllim_covs, const double[:] current_mean,
-                    const double[:] current_cov):
-    cdef Py_ssize_t Ny = FXs.shape[0]
-    cdef Py_ssize_t Ns = FXs.shape[1]
-    cdef Py_ssize_t D =  FXs.shape[2]
-    cdef Py_ssize_t K =  gllim_covs.shape[0]
-    cdef Py_ssize_t L =  gllim_covs.shape[1]
-
-    tmp_mu = np.zeros((NUM_THREADS,D))
-    ws = np.zeros((Ny,Ns))
-    maximal_mu = np.zeros(D)
-    log_p_tilde = np.zeros((NUM_THREADS,Ns))
-    tmp_KNs = np.zeros((NUM_THREADS, K,Ns))
-    tmp_KL = np.zeros((NUM_THREADS, K,L))
-    y_moins_mu = np.zeros((NUM_THREADS,D))
-
-    gllim_chol_covs = np.zeros((K, L, L))
-    for k in range(K):
-        gllim_chol_covs[k] = np.linalg.cholesky(gllim_covs[k])
-
-    _run_C_mu_step_diag_IS(Yobs, Xs, meanss, weightss, FXs, mask,
-                              gllim_covs, current_mean, current_cov,
-                              y_moins_mu, log_p_tilde, gllim_chol_covs,
-                              tmp_mu, maximal_mu, ws, tmp_KNs, tmp_KL)
-
-
-    return maximal_mu, ws
 
 
 @cython.boundscheck(False)
@@ -370,6 +266,108 @@ cdef void _run_C_mu_step_full_IS(const double[:,:] Yobs, const double[:,:,:] Xs,
 
 
 
+# ----------------------------------- Entry points ----------------------------------- #
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+def mu_step_NoIS(const double[:,:] Yobs, const double[:,:,:] FXs, const long[:,:] mask):
+    cdef Py_ssize_t Ny = FXs.shape[0]
+    cdef Py_ssize_t Ns = FXs.shape[1]
+    cdef Py_ssize_t D =  FXs.shape[2]
+
+    tmp_muNp = np.zeros(D)
+    wsNp = np.ones((Ny,Ns)) / Ns
+    maximal_muNp = np.zeros(D)
+
+    cdef double[:] tmp_mu = tmp_muNp
+    cdef double[:,:] ws = wsNp
+    cdef double[:] maximal_mu = maximal_muNp
+
+
+    cdef Py_ssize_t i, j, d
+    cdef double sum = 0
+
+    for i in range(Ny):
+        mu_helper_clean(FXs[i],mask[i],ws[i],Yobs[i], tmp_mu)
+
+        for d in range(D):
+            maximal_mu[d] += tmp_mu[d]
+
+    scalar_mult_vect(1. / Ny,maximal_mu)
+    return maximal_muNp
+
+
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+def sigma_step_full_NoIS(const double[:,:] Yobs, const double[:,:,:] FXs, const long[:,:] mask, const double[:] maximal_mu):
+    cdef Py_ssize_t Ny = FXs.shape[0]
+    cdef Py_ssize_t Ns = FXs.shape[1]
+    cdef Py_ssize_t D =  FXs.shape[2]
+
+    tmp_sigma = np.zeros((D, D))
+    ws = np.ones((Ny,Ns)) / Ns
+    maximal_sigma = np.zeros((D, D))
+    tmp = np.zeros(D)
+
+    _run_sigma_step_full(Yobs, FXs, mask, maximal_mu, ws, tmp, tmp_sigma, maximal_sigma)
+
+    return maximal_sigma
+
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+def sigma_step_diag_NoIS(const double[:,:] Yobs, const double[:,:,:] FXs, const long[:,:] mask, const double[:] maximal_mu):
+    cdef Py_ssize_t Ny = FXs.shape[0]
+    cdef Py_ssize_t Ns = FXs.shape[1]
+    cdef Py_ssize_t D =  FXs.shape[2]
+
+    esp_sigmaNp = np.zeros(D)
+    wsNp = np.ones((Ny,Ns)) / Ns
+    maximal_sigmaNp = np.zeros(D)
+    tmp_sigmaNp = np.zeros(D)
+
+    _run_sigma_step_diag(Yobs, FXs, mask, maximal_mu, wsNp, tmp_sigmaNp, maximal_sigmaNp)
+
+    return maximal_sigmaNp
+
+
+# ---- WITH IS ---- #
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+def mu_step_diag_IS(const double[:,:] Yobs, const double[:,:,:] Xs, const double[:,:,:] meanss,
+                    const double[:,:] weightss, const double[:,:,:] FXs, const long[:,:] mask,
+                    const double[:,:,:] gllim_covs, const double[:] current_mean,
+                    const double[:] current_cov):
+    cdef Py_ssize_t Ny = FXs.shape[0]
+    cdef Py_ssize_t Ns = FXs.shape[1]
+    cdef Py_ssize_t D =  FXs.shape[2]
+    cdef Py_ssize_t K =  gllim_covs.shape[0]
+    cdef Py_ssize_t L =  gllim_covs.shape[1]
+
+    tmp_mu = np.zeros((NUM_THREADS,D))
+    ws = np.zeros((Ny,Ns))
+    maximal_mu = np.zeros(D)
+    log_p_tilde = np.zeros((NUM_THREADS,Ns))
+    tmp_KNs = np.zeros((NUM_THREADS, K,Ns))
+    tmp_KL = np.zeros((NUM_THREADS, K,L))
+    y_moins_mu = np.zeros((NUM_THREADS,D))
+
+    gllim_chol_covs = np.zeros((K, L, L))
+    for k in range(K):
+        gllim_chol_covs[k] = np.linalg.cholesky(gllim_covs[k])
+
+    _run_C_mu_step_diag_IS(Yobs, Xs, meanss, weightss, FXs, mask,
+                              gllim_covs, current_mean, current_cov,
+                              y_moins_mu, log_p_tilde, gllim_chol_covs,
+                              tmp_mu, maximal_mu, ws, tmp_KNs, tmp_KL)
+
+    print(maximal_mu, ws)
+
+    return maximal_mu, ws
+
+
 @cython.boundscheck(False)
 @cython.wraparound(False)
 def mu_step_full_IS(const double[:,:] Yobs, const double[:,:,:] Xs, const double[:,:,:] meanss,
@@ -402,7 +400,6 @@ def mu_step_full_IS(const double[:,:] Yobs, const double[:,:,:] Xs, const double
                               y_moins_mu, log_p_tilde, gllim_chol_covs,
                               tmp_mu, maximal_mu, ws, tmp_KNs, tmp_KL)
 
-
     return maximal_mu, ws
 
 
@@ -432,8 +429,7 @@ def sigma_step_full_IS(Yobs, FXs, ws, mask, maximal_mu):
 
 
 
-
-
+# ------------------------------- DEBUG ------------------------------- #
 def test(X, weights, means, gllim_chol_covs, log_p_tilde, FX, mask_x, y):
     Ns = FX.shape[0]
     K, L = means.shape
